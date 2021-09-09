372274e2f73fbb2713749108497f42e07e89fa53
Juan Camilo Garcia <camilo456garcia@gmail.com>
2021-09-09 14:14:09 -0500

    favorite component

---
+++ src/components/Copy.vue
@@ -0,0 +1,28 @@
+<template>
+  <button>Share to my friends</button>
+</template>
+
+<script>
+export default {
+  name: "copy",
+}
+</script>
+
+<style lang="scss">
+
+  button{
+    color: white;    
+    background-color: var(--colorBtn);
+    padding: 10px 20px;
+    border-radius: 15rem;
+    text-decoration: none;
+    font-size: 1.8rem;
+    font-family: var(--font);
+    font-weight: 600;
+    border: none;
+    cursor: pointer;
+    &:hover{
+      background-color: var(--colorBtnHover);
+    }
+  }
+</style>
---
+++ src/components/Favorite.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <img src="../assets/img/Stargrey.svg" @click="addFav()" v-if="!isActive"/>
+  <img src="../assets/img/Staractive.svg" @click="addFav()" v-else />
+
+</template>
+
+<script>
+import { computed, ref } from '@vue/reactivity'
+import { useStore } from 'vuex'
+import { onMounted } from '@vue/runtime-core'
+export default {
+  props: ["pokemon"],
+  setup(props){
+    const store = useStore()
+
+    const pokemon  = props.pokemon
+    const isActive = computed(()=> {
+      return props.pokemon.favorite
+    })
+    console.log( props.pokemon.favorite)
+    const addFav = () => {
+      
+      pokemon.favorite = !pokemon.favorite
+      store.dispatch('pokemonFav',)
+    }
+    return{
+      addFav, isActive
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
