b0c720fa4f0f8d162164cfccc4808a1bd43b6134
Juan Camilo Garcia <camilo456garcia@gmail.com>
2021-09-08 23:16:33 -0500

    search component

---
+++ src/components/Search.vue
@@ -0,0 +1,25 @@
+<template>
+  <input type="text" class="input__search" placeholder="Pokemon">
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+  .input__search{
+    width: 80%;
+    border: none;
+    font-family: var(--font);
+    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.04);
+    padding: 14px;
+    outline-color: var(--colorBtn);
+    &::placeholder{
+      font-size: 16px;
+      font-family: var(--font);
+    }
+  }
+</style>
