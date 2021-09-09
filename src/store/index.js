a6392af0c5e9e3cb27aa40cb33b88cb4fb4ad776
Juan Camilo Garcia <camilo456garcia@gmail.com>
2021-09-09 06:20:07 -0500

    get pokemon store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,34 @@
 import { createStore } from 'vuex'
 
+const axios = require('axios');
+
+
 export default createStore({
   state: {
+    pokemon: [],
+    pokemonFav: []
   },
   mutations: {
+    setPokemon(state, payload){
+      state.pokemon = payload
+    },
+    setPokemonFav(state, payload){
+      state.pokemonFav = payload
+    }
   },
   actions: {
+    async getPokemon({ commit }){
+      try {
+        const res = await axios.get("https://pokeapi.co/api/v2/pokemon")
+        const data =  res.data.results
+        commit( "setPokemon", data)
+
+        
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
   },
   modules: {
   }
