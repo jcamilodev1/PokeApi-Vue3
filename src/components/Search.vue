db8fafe2927ccd2e10423d97b06f996d8e5b4c58
Juan Camilo Garcia <camilo456garcia@gmail.com>
2021-09-08 23:21:39 -0500

    add icon lens to search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -12,10 +12,16 @@ export default {
 <style lang="scss">
   .input__search{
     width: 80%;
+    background-image: url("../assets/img/Lens.svg");
+    background-repeat: no-repeat;
+    background-position: 5% center;
+    background-size: 15px;
     border: none;
-    font-family: var(--font);
+    border-radius: 5px;
     box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.04);
     padding: 14px;
+    padding-left: 40px;
+    font-family: var(--font);
     outline-color: var(--colorBtn);
     &::placeholder{
       font-size: 16px;
