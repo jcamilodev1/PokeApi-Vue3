9681fb05abf6773d52ccae610dd882be2ed8701b
Juan Camilo Garcia <camilo456garcia@gmail.com>
2021-09-09 15:16:35 -0500

    navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="nav">
+    <btn-icon></btn-icon>
+  </nav>
+
+</template>
+
+<script>
+import btnIcon from './btnIcon.vue'
+export default {
+
+  components: { btnIcon },
+
+}
+</script>
+
+<style lang="scss">
+  .nav{
+    padding: 20px;
+    position: sticky;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    background-color: white;
+    box-shadow: 0px -5px 4px rgba(0, 0, 0, 0.05);
+    button{
+      &:first-child{
+        margin-right: 20px;
+      }
+    }
+  }
+</style>
---
+++ src/components/btnIcon.vue
@@ -0,0 +1,68 @@
+<template>
+  <button @click="all()" :class="statusAll ? '' : 'disabled' " ><img src="../assets/img/all.svg">All</button>
+  <button @click="fav()" :class="statusFav ? '' : 'disabled' "><img src="../assets/img/fav.svg">Favorites</button>
+</template>
+
+<script>
+import { computed, ref } from '@vue/reactivity'
+import { useStore } from 'vuex'
+export default {
+    props: {
+  },
+  setup(){
+    const store = useStore()
+    const statusAll = ref(true)
+    const statusFav = ref(false)
+
+    const fav = () => {
+      statusAll.value = false
+      statusFav.value = true
+      store.dispatch('showPokemon', true)
+    }
+    const all = () => {
+      statusAll.value = true
+      statusFav.value = false
+      store.dispatch('showPokemon', false)
+    }
+    const disable = computed(() => {
+      status.value
+    })
+    return {
+      fav, all, statusAll, statusFav
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+  button{
+    width: 50%;
+    max-width: 240px;
+    color: white;    
+    background-color: var(--colorBtn);
+    padding: 10px 20px;
+    border-radius: 15rem;
+    text-decoration: none;
+    font-size: 1.8rem;
+    font-family: var(--font);
+    font-weight: 600;
+    border: none;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    min-width: 140px;
+    img{
+      width: 15px;
+      margin-right: 10px;
+    }
+    &:hover{
+      background-color: var(--colorBtnHover);
+    }
+  }
+  button.disabled{
+    background-color: #BFBFBF;
+  }
+</style>
