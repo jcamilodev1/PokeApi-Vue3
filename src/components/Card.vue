e2645f936478225e9f2a98e7ae7a495e8e8f3577
Juan Camilo Garcia <camilo456garcia@gmail.com>
2021-09-08 23:58:11 -0500

    pokemo card

---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<template>
+  <article class="card">
+    <h2>Pokemon #1</h2>
+    <img src="../assets/img/Stargrey.svg"/>
+  </article>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+  .card{
+    width: 80%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    box-shadow: var(--shadow);
+    border-radius: 5px;    
+    margin: 100px auto;
+    h2{
+      font-family: var(--font);
+      font-size: 2.2rem;
+      color: var(--colorTitle);
+    }
+  }
+</style>
