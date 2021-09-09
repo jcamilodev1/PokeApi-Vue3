87a719b65dc67b3aacd5e283d2d82e0afb49cfcc
Juan Camilo Garcia <camilo456garcia@gmail.com>
2021-09-09 05:43:14 -0500

    add axios

--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import './assets/css/style.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+Vue.use(VueAxios, axios)
 createApp(App).use(store).use(router).mount('#app')
