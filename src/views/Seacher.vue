fea3bd5cc92f4fdbd64fc8dd302d734033800a84
Juan Camilo Garcia <camilo456garcia@gmail.com>
2021-09-08 23:16:47 -0500

    page search

---
+++ src/views/Seacher.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+
+    <!-- <loader></loader> -->
+  <search></search>
+
+  </section>
+
+</template>
+
+<script>
+import Loader from '../components/Loader.vue'
+import Search from '../components/Search.vue'
+export default {
+  components: { Loader, Search },
+  name: "Seacher"
+}
+</script>
+
+<style lang="scss">
+  .container{
+    padding: 20px;
+  }
+
+</style>
